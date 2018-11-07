78db4e94a8c64e4bc6b78654bea6757ce2288e15
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-11-07 10:31:33 -0500

    Se puede visualizar elementos de matriz

--- PDE.cpp
+++ PDE.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
 #include <math.h>
 
 #define N 50  //tamaño de la matriz 
@@ -9,42 +11,60 @@
 #define TEMP_VARILLA 100.0 //temperatura permanente de la varilla
 #define TEMP_FRONTERA 10.0 // temperatura de condicion de frontera
 
+using namespace std;
+
+int posicion(int i, int j);
 void iniciales(double *matriz);
+
+
 
 
 double COEF=K/(CP*ROH); //coeficiente de difusion
 
 int main(){
   
-    double *T_ANTES = malloc(N*N*sizeof(double)); 
-    double *T_DESPUES = malloc(N*N*sizeof(double));
+    double *T_ANTES = (double*)malloc(N*N*sizeof(double)); 
+    double *T_DESPUES =(double*)malloc(N*N*sizeof(double));
     double dx=0.5; //paso espacial
     double dt=0.01; //paso temporal
     double alpha =(dt*COEF)/(dx*dx); 
-      
+    iniciales(T_ANTES);
+  
+    int i, j, posi;
+
+	  for (i = 0; i < N; i++)
+	  {
+		  for (j = 0; j < N; j++)
+		    {
+			    posi = posicion(i, j);
+			    cout<<T_ANTES[posi]<<" " ;
+
+		    }
+		  cout << endl;
+	  }
     return 0;
     
 }
 
-void iniciales(double *matriz){
-      int i,j;
-  
-      for(i = 0; i < N; i++)
-      {
-       
-            for(j = 0; j < N; j++)
-            {
-              
-              
-            }
-        
-        
-        
-      }
-  
-      
-  
-  
+int posicion(int i, int j)
+{
+	return i + j * N;
+}
+
+void iniciales(double *matriz) {
+
+	int i, j, posi;
+
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			posi = posicion(i, j);
+			matriz[posi] = TEMP_FRONTERA;
+			
+		}
+	}
+
 }
 
 
