57edc3adc5f6c889d2d6c2e78b11835cf7a12b5c
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-11-09 20:24:41 -0500

    Update hw4.mk

--- hw4.mk
+++ hw4.mk
@@ -1,1 +1,17 @@
+Resultados_hw4.pdf: 45_grados.pdf angulos.pdf Resultados_hw4.tex
+	pdflatex Resultados_hw4.tex
 
+45_grados.pdf: 45.txt Plots_hw4.py
+	python Plots_hw4.py
+
+angulos.pdf: angulo.txt Plots_hw4.py
+	python Plots_hw4.py
+
+45.txt: a.out
+	./a.out 
+
+angulo.txt: a.out 
+	./a.out 
+
+a.out: ODE.cpp
+	g++ ODE.cpp
