d8ba7bf4f5c49f00378f31bd5cb151e077f7d2a5
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-11-18 11:44:10 -0500

    Se agregan graficas de evolucion

--- Plots_hw4.py
+++ Plots_hw4.py
@@ -2,17 +2,35 @@ import numpy as np
 import matplotlib.pylab as plt
 from mpl_toolkits import mplot3d
 
+def funcion(a,b,m):
+    M=np.zeros((50,50))
+    k=0
+    for i in range(a,b):
+        
+        for j in range(50):
+            
+            M[k,j]=m[i,j]
+        
+        k=k+1
+    return M
+
+
+
+
+
+
 
 datos=np.loadtxt("45.txt")
 dat=np.loadtxt("angulo.txt")
-promedio=np.loadtxt("promedios.txt")
-tiempo=np.linspace(0.0,65,1896)
+promedio=np.loadtxt("Temperatura_promedio.txt")
+tiempo=np.linspace(0.0,0.8,23331)
 
 temp_inicial = np.loadtxt("Temperatura_inicial.txt")
 fijas=np.loadtxt("Fronteras_fijas.txt")
 abiertas=np.loadtxt("Fronteras_abiertas.txt")
 periodicas=np.loadtxt("Fronteras_periodicas.txt")
 
+g=np.loadtxt("Graficas.txt")
 
 lado_x= np.linspace(0, 50, 50)
 lado_y = np.linspace(0, 50, 50)
@@ -44,7 +62,7 @@ plt.savefig("Angulos.jpg")
 
 plt.figure()
 plt.plot(x,y)
-plt.title("Con angulo de 45 grados")    
+plt.title("Trayectoria con angulo de 45 grados")    
 plt.ylabel("y [m]")    
 plt.xlabel("x [m]") 
 plt.savefig("45.jpg")
@@ -53,6 +71,95 @@ plt.savefig("45.jpg")
 
 
 #PARTE PARA GRAFICAR LA TEMPERATURA
+
+m1=funcion(0,50,g)
+m2=funcion(50,100,g)
+m3=funcion(100,150,g)
+m4=funcion(150,200,g)
+m5=funcion(200,250,g)
+m6=funcion(250,300,g)
+
+
+plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot_surface(X, Y, m1 , rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('Temperatura [K]')
+ax.set_zlim3d(283.15,373.15)
+ax.set_title('Primera evolucion fronteras fijas')
+plt.savefig("1_fijas.jpg")
+
+
+
+
+plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot_surface(X, Y, m2 , rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('Temperatura [K]')
+ax.set_zlim3d(283.15,373.15)
+
+ax.set_title('Segunda evolucion fronteras fijas')
+plt.savefig("2_fijas.jpg")
+
+
+
+plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot_surface(X, Y, m3 , rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('Temperatura [K]')
+ax.set_zlim3d(283.15,373.15)
+ax.set_title('Primera evolucion fronteras abiertas')
+plt.savefig("1_abiertas.jpg")
+
+
+plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot_surface(X, Y, m4 , rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('Temperatura [K]')
+ax.set_zlim3d(283.15,373.15)
+ax.set_title('Segunda evolucion fronteras abiertas')
+plt.savefig("2_abiertas.jpg")
+
+
+
+plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot_surface(X, Y, m5 , rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('Temperatura [K]')
+ax.set_zlim3d(283.15,373.15)
+ax.set_title('Primera evolucion fronteras periodicas')
+plt.savefig("1_periodicas.jpg")
+
+
+plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot_surface(X, Y, m6 , rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('Temperatura [K]')
+ax.set_zlim3d(283.15,373.15)
+ax.set_title('Segunda evolucion fronteras periodicas')
+plt.savefig("2_periodicas.jpg")
+
+
+
+
+
 
 plt.figure()
 ax = plt.axes(projection='3d')
