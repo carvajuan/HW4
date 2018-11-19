a7fe1a980ae815e9dd37489cba971d98c4fbd9f5
carvajuan <42377494+carvajuan@users.noreply.github.com>
2018-11-18 22:21:57 -0500

    Se agregan todas las dependencias

--- hw4.mk
+++ hw4.mk
@@ -1,17 +1,20 @@
-Resultados_hw4.pdf: 45_grados.pdf angulos.pdf Resultados_hw4.tex
+Resultados_hw4.pdf: 45.jpg Angulos.jpg 1_fijas.jpg 2_fijas.jpg 1_abiertas.jpg 2_abiertas.jpg 1_periodicas.jpg 2_periodicas.jpg Momento_inicial.jpg Fronteras_fijas.jpg Fronteras_abiertas.jpg Fronteras_periodicas.jpg Temperatura_promedio.jpg Resultados_hw4.tex
 	pdflatex Resultados_hw4.tex
 
-45_grados.pdf: 45.txt Plots_hw4.py
+45.jpg Angulos.jpg: 45.txt angulo.txt Plots_hw4.py
 	python Plots_hw4.py
 
-angulos.pdf: angulo.txt Plots_hw4.py
+1_fijas.jpg 2_fijas.jpg 1_abiertas.jpg 2_abiertas.jpg 1_periodicas.jpg 2_periodicas.jpg Momento_inicial.jpg Fronteras_fijas.jpg Fronteras_abiertas.jpg Fronteras_periodicas.jpg Temperatura_promedio.jpg: Temperatura_promedio.txt Temperatura_inicial.txt Fronteras_fijas.txt Fronteras_abiertas.txt Fronteras_periodicas.txt Graficas.txt Plots_hw4.py
 	python Plots_hw4.py
 
-45.txt: a.out
-	./a.out 
+45.txt angulo.txt: punto1.x
+	./punto1.x
 
-angulo.txt: a.out 
-	./a.out 
-
-a.out: ODE.cpp
-	g++ ODE.cpp
+Temperatura_promedio.txt Temperatura_inicial.txt Fronteras_fijas.txt Fronteras_abiertas.txt Fronteras_periodicas.txt Graficas.txt: punto2.x
+	./punto2.x
+	
+punto1.x : ODE.cpp
+	g++ ODE.cpp -o punto1.x
+	
+punto2.x: PDE.cpp
+	g++ PDE.cpp -o punto2.x
